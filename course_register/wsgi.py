fb7637a4759ec63c842538e5e75f16163071480a
Izzet-Aghayev <agayevizzet@gmail.com>
2026-09-08 21:59:50 +0400

    Vercel automatic migration support

--- course_register/wsgi.py
+++ course_register/wsgi.py
@@ -1,9 +1,14 @@
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'course_register.settings')
 
-application = get_wsgi_application()
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Automatic migration error: {e}", file=sys.stderr)
 
+application = get_wsgi_application()
 app = application
